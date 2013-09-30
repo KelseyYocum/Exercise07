a24ae7d112e1c9d7ce5807b168c7f95fbae2ad94
Hackbright Student <user@chromebox08.(none)>
2013-09-30 15:22:30 -0700

    new method for extracting lines & stripping extra spaces off of lines.

--- sorted_data.py
+++ sorted_data.py
@@ -1,22 +1,25 @@
 from sys import argv
 script, filename = argv
 
-# opening and reading the file. Splitting on the line breaks
-lines = open(filename).read().split('\n')
+def main():
+    # opening and reading the file. Splitting on the line breaks
+    lines = open(filename).readlines()
 
-# creating a list of lists, splitting on the colon.
-scores_list = []
-for i in lines:
-    scores_list.append(i.split(":"))
+    # creating a list of lists, splitting on the colon.
+    scores_list = []
+    for line in lines:
+        line = line.strip()
+        scores_list.append(line.split(":"))
 
-# creating a dictionary out of the list of lists
-scores_dictionary = dict(scores_list)
+    # creating a dictionary out of the list of lists
+    scores_dictionary = dict(scores_list)
 
-# alphabetizing dictionary keys
-sorted_keys = sorted(scores_dictionary.keys())
+    # alphabetizing dictionary keys
+    sorted_keys = sorted(scores_dictionary.keys())
 
-# printing alphabetized keys and values
-for key in sorted_keys:
-    print "%s is rated at %s." % (key, scores_dictionary[key])
+    # printing alphabetized keys and values
+    for key in sorted_keys:
+        print "%s is rated at %s." % (key, scores_dictionary[key])
 
 
+main()
